966c03f8ea7d63c279c6306bc9c2e187f2432059
afisher92 <fisheand92@gmail.com>
2015-12-14 10:27:02 -0600

    Update LED.sv

--- LED.sv
+++ LED.sv
@@ -1,12 +1,13 @@
 module LED (
 	input clk,
 	input rst_n,
-	input [15:0] rht_out,
-	input [15:0] lft_out,
+	input signed [15:0] rht_out,
+	input signed [15:0] lft_out,
 	output reg [7:0] LED
 );
 
 reg [31:0] cnt;
+
 
 always @(posedge clk, negedge rst_n) begin
 	if(!rst_n)
@@ -20,12 +21,12 @@ end
 always @(posedge cnt[25], negedge rst_n) begin
 	if(!rst_n)
 		LED[3:0] <= 4'h0;
-	else if(~&rht_out[15:4])
-		LED[3:0] <= 4'b1100;
-	else if(~&rht_out[15:8])
-		LED[3:0] <= 4'b1110;
-	else if(~&rht_out[15:12])
+	else if(rht_out>16'hf000)
 		LED[3:0] <= 4'b1111;
+	else if(rht_out >16'h0f00 )
+		LED[3:0] <= 4'b1110;
+	else if(rht_out>16'h00f0)
+		LED[3:0] <= 4'b1100;
 	else
 		LED[3:0] <= 4'b1000;
 end
@@ -33,12 +34,12 @@ end
 always @(posedge cnt[25], negedge rst_n) begin
 	if(!rst_n)
 		LED[7:4] <= 4'h0;
-	else if(~&lft_out[15:4])
-		LED[7:4] <= 4'b0011;
-	else if(~&lft_out[15:8])
-		LED[7:4] <= 4'b0111;
-	else if(~&lft_out[15:12])
+	else if(lft_out >16'hf000)
 		LED[7:4] <= 4'b1111;
+	else if(lft_out > 16'h0f00)
+		LED[7:4] <= 4'b0111;
+	else if(lft_out > 16'h00f0)
+		LED[7:4] <= 4'b0011;
 	else
 		LED[7:4] <= 4'b0001;
 end
